330a5664a9364e0eb2679f85aa012ce718b3e0de
Kyle Somerfeldt <kyle.somerfeldt@gmail.com>
2018-11-01 21:56:36 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
  
-mongoose.connect("mongodb://localhost/yelp_camp_v6", { useNewUrlParser:true });
+// mongoose.connect("mongodb://localhost/yelp_camp_v6", { useNewUrlParser:true });
+mongoose.connect("mongodb://kyle:radish1@ds249123.mlab.com:49123/yelpcampkyle");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
