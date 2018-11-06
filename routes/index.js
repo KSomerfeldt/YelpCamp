c1c2f5a57682c8a86d89105da693ada9786cea6f
Kyle Somerfeldt <kyle.somerfeldt@gmail.com>
2018-11-06 02:52:04 +0000

    bootstrap 4 update

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
+
 //root route
 router.get("/", function(req, res){
    res.render("landing");
@@ -37,9 +38,10 @@ router.get("/login", function(req, res){
 router.post("/login", passport.authenticate("local",
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: 'Welcome to Yelpcamp!'
     }), function(req, res){
-   
 });
 
 
